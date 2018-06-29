673e2e60b1d13016be6d3bb168906e86a74269bf
nieshankun <38554448+nieshankun@users.noreply.github.com>
2018-06-29 18:02:02 +0800

    eureka service register

--- src/main/java/com/nsk/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
+++ src/main/java/com/nsk/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
@@ -2,7 +2,9 @@ package com.nsk.cloud.microservicesimpleprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviceSimpleProviderUserApplication {
 
